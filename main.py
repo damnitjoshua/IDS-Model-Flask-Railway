0493470ef787cfa7ccbff51a6ca7af9bbd39c56e
Joshua Chew <joshuachew8118@gmail.com>
2023-01-18 02:27:41 +0800

    test

--- main.py
+++ main.py
@@ -1,67 +1,12 @@
 from flask import Flask, jsonify
 import os
-import numpy as np
-import pandas as pd
-from joblib import load
 
 app = Flask(__name__)
-model = load("model/knn_bookRecom_model.sav")
-
-
-# Rating count more than 50
-csv_url = "https://drive.google.com/file/d/1tIxt00bOAPEKRkc57uuBhPZGDLBwsvJc/view?usp=share_link"
-csv_url = 'https://drive.google.com/uc?id=' + csv_url.split('/')[-2]
-RatingCountDF = pd.read_csv(csv_url)
-RatingCountDFPivot = RatingCountDF.pivot(
-    index='ISBN', columns='UserID', values='Rating').fillna(0)
 
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
-
-@app.route('/random/<count>')
-def random(count):
-    books = []
-
-    for i in range(int(count)):
-        query_index = np.random.choice(RatingCountDF.shape[0])
-        book = RatingCountDF.iloc[query_index]
-        books.append({
-            "title": book['Title'],
-            "author": book['Author'],
-            "isbn": book['ISBN'],
-            "image": book['Image'],
-            "category": book["Category"][2:-2],
-            "rating": str(book['Rating']),
-            "ratingCount": str(book["RatingCount"]),
-            "yearOfPublication": str(book["YearOfPublication"])
-        })
-
-    return books
-
-
-@app.route('/knn/<ISBN>')
-def knn(ISBN):
-    search = RatingCountDFPivot.loc[ISBN]
-
-    distances, indices = model.kneighbors(
-        search.values.reshape(1, -1), n_neighbors=6)
-
-    books = []
-
-    for i in range(0, len(distances.flatten())):
-        if i != 0:
-            book = RatingCountDF.iloc[indices.flatten()[i]]
-            books.append({
-                "title": book['Title'],
-                "isbn": book['ISBN'],
-                "image": book['Image'],
-                "distance": distances.flatten()[i]
-            })
-
-    return books[::-1]
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 if __name__ == '__main__':
